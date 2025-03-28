75fdf1650b66448e91c96c9361c3dfdd2516c8cd
Abdullayeva <la.abdullayeva99@gmail.com>
2025-03-28 15:32:51 +0100

    new home, gallery, shop components +, app component: nav added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./pages/shop/shop.component').then((m) => m.ShopComponent),
+  },
+  {
+    path: 'gallery',
+    loadComponent: () =>
+      import('./pages/gallery/gallery.component').then(
+        (m) => m.GalleryComponent
+      ),
+  },
+];
---
+++ client/src/app/pages/gallery/gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  imports: [],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.css',
+  standalone: true,
+})
+export class GalleryComponent {}
